0d8b3c3f0aba9c443e0ecf906fbba5d13611ed74
piasmajumdar <piasmajumdar@gmail.com>
2026-04-21 19:48:05 +0530

    Image Loading From url.. Config Image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  // https://i.ibb.co/WNDdTBvV/butterfly.avif
   reactCompiler: true,
+  images: {
+    // domains: ['images.unsplash.com', 'i.ibb.co'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+
+  },
 };
 
 export default nextConfig;
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,16 @@
 // import React from 'react';
+import butterfly from "../../assets/images/butterfly.avif"
+
+import Image from "next/image";
 
 const AboutPage = () => {
     return (
         <div>
             <h2>This is About Page</h2>
+            <Image src="https://i.ibb.co/WNDdTBvV/butterfly.avif" alt="Butterfly" width="500" height="300"></Image>
+            <Image src='/nature.jpg' alt="Nature" width="500" height="300"></Image>
+            <Image src={butterfly} alt="Butterfly" width="500" height="300"></Image>
+            <Image src="https://images.unsplash.com/photo-1773332611613-dbdc64f09807?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Butterfly" width="500" height="300"></Image>
         </div>
     );
 };
